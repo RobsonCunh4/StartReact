1f9b688f8520c4a8644fef4c80afe96b42484d02
Robson Cunha <rcunha.sm@gmail.com>
2022-05-19 13:27:23 -0300

    StartReact
    Aula.

--- views/Login.js
+++ views/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { KeyboardAvoidingView, Platform, View, Text, Image, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { css } from '../assets/css/Css';
@@ -6,6 +6,31 @@ import { css } from '../assets/css/Css';
 export default function Login() {
 
     const [display, setDisplay] = useState('none');
+    const [user, setUser] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [login, setLogin] = useState(null);
+
+    async function sendForm() {
+        let response = await fetch('http://localhost:3000/login', {
+
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: user,
+                password: password
+            })
+        });
+        let json=await response.json();
+        if(json === 'error'){
+            setDisplay('flex');
+            setTimeout(()=>{
+                setDisplay('none');
+            }, 5000)
+        }
+    }
 
     return (
         <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={[css.container, css.darkbg]}>
@@ -18,9 +43,9 @@ export default function Login() {
             </View>
 
             <View style={css.login__form}>
-                <TextInput style={css.login__input} placeholder='Usuário:' />
-                <TextInput style={css.login__input} placeholder='Senha:' secureTextEntry={true} />
-                <TouchableOpacity style={css.login__button} onPress={()=>setDisplay('flex')}>
+                <TextInput style={css.login__input} placeholder='Usuário:' onChangeText={text => setUser(text)} />
+                <TextInput style={css.login__input} placeholder='Senha:' secureTextEntry={true} onChangeText={text => setPassword(text)} />
+                <TouchableOpacity style={css.login__button} onPress={() => sendForm()}>
                     <Text style={css.login__buttonText}>Login</Text>
                 </TouchableOpacity>
             </View>
